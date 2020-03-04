89fc976fe7f059284f9957b404d23bd3417806ac
mounikabhaskar <mounikabhaskar.kolar@gmail.com>
2020-03-04 21:24:39 +0530

    Images saving locally

--- TMDB_Task/UIImageView+Extensions.swift
+++ TMDB_Task/UIImageView+Extensions.swift
@@ -13,11 +13,23 @@ extension UIImageView {
    func loadImageFrom(urlLastPath: String) {
       self.image = nil
       let tmdbImagePath = "http://image.tmdb.org/t/p/w185"
+      
       DispatchQueue.global(qos: .userInitiated).async {
          let imageUrlPath = tmdbImagePath + urlLastPath
-         guard let imageUrl = URL(string: imageUrlPath), let imageData = try? Data(contentsOf: imageUrl), let movieImage = UIImage(data: imageData) else { return }
-         DispatchQueue.main.async {
-            self.image = movieImage
+         guard let imageUrl = URL(string: imageUrlPath) else { return }
+
+         if let documentUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let imageLocalUrl = documentUrl.appendingPathComponent(imageUrl.lastPathComponent)
+            if FileManager.default.fileExists(atPath: imageLocalUrl.path), let image = UIImage(contentsOfFile: imageLocalUrl.path) {
+               DispatchQueue.main.async {
+                  self.image = image
+               }
+            } else if let imageData = try? Data(contentsOf: imageUrl), let movieImage = UIImage(data: imageData) {
+               try? imageData.write(to: documentUrl)
+               DispatchQueue.main.async {
+                  self.image = movieImage
+               }
+            }
          }
       }
    }
